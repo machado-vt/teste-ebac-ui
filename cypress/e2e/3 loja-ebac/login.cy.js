804192d6f95c45ec9c6587060a9b330216606380
machado.vt <machado.vt@tutanota.com>
2024-04-05 16:31:34 -0300

    Testes com massa de dados e usando fixture

--- cypress/e2e/3 loja-ebac/login.cy.js
+++ cypress/e2e/3 loja-ebac/login.cy.js
@@ -1,10 +1,11 @@
 /// <reference types="cypress"/>
+const perfil = require('../../fixtures/perfil.json')
 
 describe("Funcionalidade: Login", () => {
 
     beforeEach(() => {
 
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta/')
         
     });
 
@@ -46,5 +47,24 @@ describe("Funcionalidade: Login", () => {
         
         
     });
+
+    it('Deve fazer login com sucesso - Usando massa de dados', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()        
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, vitor.teste (não é vitor.teste? Sair)')
+        
+    });
+
+    it.only('Deve fazer login com sucesso - Usando Fixture', () => {
+        cy.fixture('perfil').then(dados => {
+            cy.get('#username').type(dados.usuario , {log: false})
+            cy.get('#password').type(dados.senha , {log: false})
+            cy.get('.woocommerce-form > .button').click()        
+            cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, vitor.teste (não é vitor.teste? Sair)')          
+        })
+        
+    });
 })
+
 
